d5b98c9dca4bdaf926566415d2743a1c7aef505e
SANVAL2024 <159937982+SANVAL2024@users.noreply.github.com>
2024-02-19 15:20:58 -0500

    Update proveedor.tf

--- proveedor.tf
+++ proveedor.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tfstatesandra"
+    key    = "tfstate"
+    region = "us-east-1"
+  }
+}
