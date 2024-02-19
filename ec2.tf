5f60f2c30e80df7fc8442d72c356819b2e83de5c
SANVAL2024 <159937982+SANVAL2024@users.noreply.github.com>
2024-02-19 16:37:40 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,26 @@
+resource "aws_security_group" "my_security_group" {
+  name        = "my_security_group"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = aws_vpc.my_vpc.id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-0e731c8a588258d0d"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet1.id
+  security_groups = [aws_security_group.my_security_group.name]
+}  
